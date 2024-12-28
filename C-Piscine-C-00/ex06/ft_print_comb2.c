40d88c1fbd58e2d16d7e7433808ca759952ee914
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2024-12-28 07:12:34 +0100

    update c00

--- C-Piscine-C-00/ex06/ft_print_comb2.c
+++ C-Piscine-C-00/ex06/ft_print_comb2.c
@@ -6,34 +6,54 @@
 /*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 03:14:14 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2024/12/28 03:55:33 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2024/12/28 07:11:02 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+static void	print_comb(char a, char b, char c, char d)
+{
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, " ", 1);
+	write(1, &c, 1);
+	write(1, &d, 1);
+	write(1, "\n", 1);
+}
+
 void	ft_print_comb2(void)
 {
-	char	d;
-	char	u;
+	char	di;
+	char	ui;
+	char	dd;
+	char	ud;
 
-	d = '0';
-	while (d <= '9')
+	di = '0';
+	while(di <= '9')
 	{
-		write(1, &d, 1);
-		u = '0';
-		while(u <= '9')
+		ui = '0';
+		while (ui <= '9')
 		{
-			write(1, &u, 1);
-			u++;
+			dd = '0';
+			while (dd <= '9')
+			{
+				ud = '0';
+				while (ud <= '9')
+				{
+					print_comb(di, ui, dd, ud);
+					ud++;
+				}
+				dd++;
+			}
+			ui++;
 		}
-		write(1, "\n", 1);
-		d++;
+		di++;
 	}
 }
 
-int main()
-{
-	ft_print_comb2();
-	return (0);
-}
+// int main()
+// {
+// 	ft_print_comb2();
+// 	return (0);
+// }
